5d2dfe6be2aa3b0eb9684c97cbc50bba58c84188
AntoDono <anthony20151128@gmail.com>
2022-08-15 23:56:43 -0400

    skills uncomplete

--- components/Skills.vue
+++ components/Skills.vue
@@ -1,19 +1,78 @@
 <template>
-    <div>
-        <skill img_name="python"/>
-        <skill img_name="java"/>
-        <skill img_name="js"/>
-        <skill img_name="c"/>
-        <skill img_name="c++"/>
-        <skill img_name="r"/>
-        <skill img_name="html"/>
-        <skill img_name="css"/>
-        <skill img_name="vue"/>
-        <skill img_name="react"/>
-        <skill img_name="nuxt"/>
-        <skill img_name="mongo"/>
-        <skill img_name="nginx"/>
-        <skill img_name="rasa"/>
-        <skill img_name="graphql"/>
+    <div class="flex flex-wrap justify-center gap-x-10 gap-y-5" ref="skills">
+        <skill img_name="python" ref="python" />
+        <skill img_name="java" ref="java" />
+        <skill img_name="js" ref="js" />
+        <skill img_name="c" ref="c" />
+        <skill img_name="c++" ref="c++" />
+        <skill img_name="r" ref="r" />
+        <skill img_name="html" ref="html" />
+        <skill img_name="css" ref="css" />
+        <skill img_name="vue" ref="vue" />
+        <skill img_name="react" ref="react" />
+        <skill img_name="nuxt" ref="nuxt" />
+        <skill img_name="mongo" ref="mongo" />
+        <skill img_name="nginx" ref="nginx" />
+        <skill img_name="rasa" ref="rasa" />
+        <skill img_name="graphql" ref="graphql" />
     </div>
 </template>
+
+<script>
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+export default {
+    data() {
+        return {
+            skills: [
+                "python",
+                "java",
+                "js",
+                "c",
+                "c++",
+                "r",
+                "html",
+                "css",
+                "vue",
+                "react",
+                "nuxt",
+                "mongo",
+                "nginx",
+                "rasa",
+                "graphql",
+            ]
+        }
+    },
+    mounted() {
+        gsap.registerPlugin(ScrollTrigger)
+
+        gsap.to(
+            this.$refs['skills'],
+            {
+                scrollTrigger: {
+                    trigger: this.$refs['skills'],
+                    markers: false,
+                },
+                onUpdate: this.show,
+                duration: 0
+            }
+        )
+    },
+    methods: {
+        show() {
+            let tl = gsap.timeline()
+            this.skills.forEach((e) => {
+                tl.to(this.$refs[e], {
+                    duration: 0.5,
+                    ease: "power1.out",
+                    onStart: ()=>{
+                        this.$refs[e].init()
+                    }
+                })
+            })
+        }
+    }
+}
+
+</script>
--- components/skill.vue
+++ components/skill.vue
@@ -1,18 +1,49 @@
 <template>
-    <div class="z-20 relative">
+    <div class="z-20 relative w-[25vw] min-w-[400px] opacity-[10%]" ref="container">
         <div>
-            <img class="logo" :src="`../assets/images/skills/${img_name}.png`"/>
+            <img class="logo" :src="`../assets/images/skills/${img_name}.png`" />
+        </div>
+        <h2 class="text-white font-uni">{{ skill_name }}</h2>
+        <div class="h-2 w-full bg-gray-500 rounded" ref="full">
+            <div class="h-full bg-blue-400 w-0" ref="load"></div>
         </div>
     </div>
 </template>
 
 <script>
+import gsap from 'gsap'
 
 export default {
     props: {
         img_name: {
             default: null,
             type: String
+        },
+        skill_name: {
+            default: "Skill name",
+            type: String
+        },
+        percentage: {
+            default: 0.75,
+            type: Number
+        }
+    },
+    methods: {
+        init() {
+            gsap.to(
+                this.$refs['container'],
+                {
+                    autoAlpha: 1,
+                    duration: 0.5
+                }
+            )
+            gsap.to(
+                this.$refs['load'],
+                {
+                    width: this.percentage * this.$refs['full'].offsetWidth,
+                    duration: 1.5
+                }
+            )
         }
     }
 }
@@ -20,8 +51,9 @@ export default {
 </script>
 
 <style scope>
-.logo{
+.logo {
     height: 50px;
     width: 50px;
+    border-radius: 5px;
 }
 </style>
