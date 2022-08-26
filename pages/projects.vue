032cf02e26e538b3c7d014e7fae4f6ac3ddbcf92
AntoDono <anthony20151128@gmail.com>
2022-08-26 19:55:35 -0400

    under development page

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
-        hi  
+    <div class="w-full h-screen bg-primary flex flex-col justify-center items-center">
+        <h1 class="text-white font-bogart text-[5vmin]">Page is still under development.</h1>
+        <navigatebutton text="Go Back" />
     </div>
 </template>
