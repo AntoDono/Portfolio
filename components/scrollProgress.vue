51f1e25ef0e7b2b88729d9013cd26912a500f152
AntoDono <anthony20151128@gmail.com>
2023-02-07 18:35:30 -0500

    basic scroll indicator

--- components/scrollProgress.vue
+++ components/scrollProgress.vue
@@ -1,12 +1,26 @@
 <template>
-    <div class="fixed right-0 top-1/2 z-30">
-        <div class="text-2xl text-white">
-            SCROLL INDICATOR
+    <div class="fixed right-5 top-0 z-30">
+        <div class="text-2xl text-white h-screen flex justify-center items-center ">
+            <div class="h-[35%] w-2 rounded bg-gray-700">
+                <div class="h-full bg-green-500 rounded" ref="progress"></div>
+            </div>
         </div>
     </div>
 </template>
 
-<script>
+<script setup>
 
+import { ref, onMounted } from 'vue'
+
+const progress = ref(null)
+
+onMounted(()=>{
+    let height = (document.documentElement.scrollHeight || document.body.scrollHeight) - window.innerHeight
+    progress.value.style.height = '0px'
+    document.addEventListener("scroll",()=>{
+        let current_height = document.documentElement.scrollTop || document.body.scrollTop
+        progress.value.style.height = `${current_height / height * 100}%`
+    })
+})
 
 </script>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <displayprojects />
+        <ScrollProgress/>
     </div>
 </template>
 
