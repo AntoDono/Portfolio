08f3f6e18903557ae4ddf834efac94580f4b3365
AntoDono <anthony20151128@gmail.com>
2023-12-01 22:03:43 -0500

    error 500 when SSR. Removed GSAP plugin

--- plugins/gsap.js
+++
@@ -1,9 +0,0 @@
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { ScrollToPlugin } from 'gsap/ScrollToPlugin'
-import gsap from "gsap"
-
-export default ({}, inject) => {
-  gsap.registerPlugin(ScrollTrigger)
-  gsap.registerPlugin(ScrollToPlugin)
-  inject('gsap', gsap)
-}
